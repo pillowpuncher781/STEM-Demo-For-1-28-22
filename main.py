9ce62e1c1964d813558d0c73914a261d19486df2
pillowpuncher781 <97123045+pillowpuncher781@users.noreply.github.com>
2022-01-27 10:50:13 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+people_who_like_pizza = 1 #write people who like pizza.
+lovers_of_hamburgers = 12  # write people who like hamburgers.
+hotdog_fanatics = 8    #write people who like hotdogs.
+if people_who_like_pizza >= 8:
+    print ("we have a lot of pizza lovers in da house, " +  str(people_who_like_pizza) + " in fact.")
+else:
+    print ("L Pizza")
+if lovers_of_hamburgers >= 8:
+    print ("You all lied, there is supposedly " + str(lovers_of_hamburgers) + " who got no cheese (aka, they like hamburgers) ")
+else:
+    print ("L hamburgers")
+
+if hotdog_fanatics >= 8:
+    print ("There are " + str(hotdog_fanatics) + " single ladies, now put your hands up.")
+else:
+    print ("L hotdogs")
